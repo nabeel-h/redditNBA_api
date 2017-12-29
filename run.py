5668e17a549431a3956ee2d90d89c9ab4b833d7b
Nabeel Hussain <nabeelh09@gmail.com>
2017-12-29 11:40:44 -0800

    Fixed not importing db when running the app.

---
+++ run.py
@@ -0,0 +1,13 @@
+from app import app
+from db import db
+
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+	db.create_all()
+	
+	#if database is already not populated then populate items
+	if db.session.query(SubmissionModel).count() < 1:
+		from fill_up_db import fill_up_db
+		print(fill_up_db())
